4ce86dfa2e9ebb15392617d6ff32f89f4976a886
Selena Singleton <ssingle7@gmail.com>
2021-01-25 18:59:17 -0500

    added additional starter files needed to build application

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,4 @@
+// import the following:
+
+//    * Express
+//    * `burger.js`
---
+++ models/burger.js
@@ -0,0 +1,2 @@
+// import `orm.js` into `burger.js`
+// create the code that will call the ORM functions using burger specific input for the ORM.
