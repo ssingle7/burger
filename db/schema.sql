324a07f085e8c9adc180b30000a4de81f59f320f
Selena Singleton <ssingle7@gmail.com>
2021-01-26 21:54:16 -0500

    modified files to correct route and node while attempting to listen on the port

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,15 @@
--- * Create the `burgers_db`.
-   /* Switch to or use the `burgers_db`.
-   * Create a `burgers` table with these fields:
-     * **id**: an auto incrementing int that serves as the primary key.
-     * **burger_name**: a string.
-     * **devoured**: a boolean.
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
 
-     
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
+);
+
+/* JAWS DB SCHEMA */
+
+
--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,27 @@
 // import `orm.js` into `burger.js`
 // create the code that will call the ORM functions using burger specific input for the ORM.
+
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+    create: function (name, cb) {
+        orm.create("burgers", [
+            "burger_name", "devoured"
+        ], [
+            name, false
+        ], cb);
+    },
+    update: function (id, cb) {
+        var condition = "id=" + id;
+        orm.update("burgers", {
+            devoured: true
+        }, condition, cb);
+    }
+};
+
+module.exports = burger;
