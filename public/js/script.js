a381af9b4366bcc34940052aa1e60e3cc2f9a429
Selena Singleton <ssingle7@gmail.com>
2021-02-06 20:41:51 -0500

    corrected mispelling for the place order button to render properly in the browser

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-
-    $(".devour-form").on("submit", function (event) {
+    console.log("script loaded");
+    $("#devour_btn").on("click", function (event) {
         event.preventDefault();
+        
 
         var burger_id = $(this).children(".burger_id").val();
         console.log(burger_id);
         $.ajax({
             method: "PUT",
-            url: "/burgers/" + burger_id
+            url: "/burger/" + burger_id
         }).then(function (data) {
             // reload page to display devoured burger in proper column
             location.reload();
